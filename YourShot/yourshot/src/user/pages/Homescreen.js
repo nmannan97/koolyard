c9c0e5dd6a6ed0296b19c257edffe9538b5654d5
Naeem Mannan <nmannan97@gmail.com>
2022-08-23 20:58:11 -0700

    general changes

--- YourShot/yourshot/src/user/pages/Homescreen.js
+++ YourShot/yourshot/src/user/pages/Homescreen.js
@@ -1,6 +1,6 @@
 import './Homescreen.css';
 import Navbar from '../../Navbar';
-
+import NoUser from '../pictures/noUser.png'
 //Images
 import Naeem from "../pictures/teammates/Naeem.jpeg"
 
@@ -15,16 +15,21 @@ function Homescreen(){
                     to get photographers to and from photo shoots
                 </div>
                 <div id="homepage_meet_the_team">
+                <p className='homepage_meet_the_team'>Meet the team</p>
                     <div className="homepage_container">
+                    
                         <div className="homepage_item">
                             <div className="homepage_polaroid">
-                                Naeem
-                                <img src={Naeem} />
-                                <br/>
-                                
+                                <p className='homepage_generic_font'>Naeem</p>
+                                <img className = "img_polaroid" src={Naeem} />                                 
                             </div>
                             <div className="homepage_polaroid">
-                                James
+                                <p className='homepage_generic_font'>James</p>
+                                <img className = "img_polaroid" src={NoUser} />   
+                            </div>
+                            <div className="homepage_polaroid">
+                                <p className='homepage_generic_font'>Yana</p>
+                                <img className = "img_polaroid" src={NoUser} />   
                             </div>
                         </div>
                     </div>
