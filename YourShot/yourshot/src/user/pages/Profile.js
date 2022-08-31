694b80b8bb1fba57b3b2aacf859bf409f9a1df43
Naeem Mannan <nmannan97@gmail.com>
2022-08-30 21:22:45 -0700

    general changes

--- YourShot/yourshot/src/user/pages/Profile.js
+++ YourShot/yourshot/src/user/pages/Profile.js
@@ -1,8 +1,19 @@
+import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+
 import './Profile.css';
 import Navbar from '../../Navbar';
 
 import noUser from "../pictures/noUser.png"
+import userProfile from '../pictures/Assets/UserProfile.png'
+import message from "../pictures/Assets/Message.png"
+import share from "../pictures/Assets/Share.png"
+import select1 from "../pictures/Assets/Edit2.png"
+import selectMain from "../pictures/Assets/Edit1.png"
+
 function Profile(){
+
+    const [userProfile, setUserProfile] = useState();
 
     const addPhotos = () => {
         let image = []
@@ -13,17 +24,38 @@ function Profile(){
         return image
     }
 
+    function editPhoto(e) {
+        userProfile = URL.createObjectURL(e.target.files[0])
+        setUserProfile(URL.createObjectURL(e.target.files[0]));
+        return 
+    }
+
     return(
         <>
         <div id="profile">
             <Navbar />
-            <p>Welcome to your client profile </p>
-            <img className="noUser" src={noUser}/>
-            <div id="client_column">
-                <div id="client_row">
-                    {addPhotos()}
+                <div id="profile_background1">
+                
+                
+                <div id="profile_background2">
+                    <img id="profile_user" src={userProfile}/>
+                    <Link to='/messages'><img id="profile_message" src={message}/></Link>
+                    
+                    <img id="profile_share" src={share}/>
+                    <input type="file" onChange={editPhoto()}>
+                        <img id="profile_select" src={selectMain}/>
+                        <img id="profile_select2" src={select1}/>
+                    </input>
+                    <div id="client_column">
+                        <div id="client_row">
+                            {addPhotos()}
+                        </div>
+                    </div>
+
                 </div>
+                
             </div>
+            
         </div>
         </>
     );
