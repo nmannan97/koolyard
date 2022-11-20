73f66df545a39a51dd4a6daf8833b50ec9f17f08
Daisy Halaszyn <daisy.halaszyn@gmail.com>
2022-11-19 16:42:25 -0800

    message

--- koolyard/src/user/pages/Homescreen/About.js
+++ koolyard/src/user/pages/Homescreen/About.js
@@ -8,14 +8,7 @@ export default function About(){
                     SkoolYard is an organization that thrives on peer-to-peer education.
                     <br/><br/> </p>
                 <p style={{paddingBottom:"20px"}}>
-                    The SkoolYard is a new technology focusing around blockchanin NFT technology that allows
-                    creators and students to collect income for educational material they create. Thus giving 
-                    people of merit more ability to make more earnings for being educational. This allows for 
-                    students and educators to sell their educational material at a price, similar to open seas
-                    and rarible NFTs, and therefore make cryptocurrency off the material they sold to people 
-                    who want to learn a subject off them. Then to make sure that children are getting well educated
-                    and not scammed, parents will buy these "NFTs" for a limited time to forever for prices 
-                    educators or students set them to. 
+                    hello 
                 </p>
             </div>
         </div>
