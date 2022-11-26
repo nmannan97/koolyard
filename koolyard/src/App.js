89a9fa0abbfd9d971a9b847f9045e56bb6b155b5
Naeem Mannan <nmannan97@gmail.com>
2022-11-26 12:51:41 -0800

    adding changes

--- koolyard/src/App.js
+++ koolyard/src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       <div className="menu">   
         <Routes>
           <Route exact path='' element={<Homepage/>} />
-          <Route exact path='master.d1p7cqoiehhyre.amplifyapp.com/' element={<Homepage/>} />
+          <Route exact path='/' element={<Homepage/>} />
           <Route exact path='/messages' element={<Messages/>}/>
           <Route exact path='/profile' element={<Profile/>}/>
           <Route exact path='/signupsignin' element={<SignupSignin/>}/>
